5cd01bae5bdf9ddd19601b31ec4f5079fb7781ef
shorif <shorif.tk@gmail.com>
2023-12-23 19:49:09 +0600

    some changes

--- app/Http/Controllers/TripController.php
+++ app/Http/Controllers/TripController.php
@@ -22,15 +22,36 @@ class TripController extends Controller
             'to' => 'required|string',
             'paribahan_name' => 'required|string',
             'total_seats' => 'required|integer',
-            'date_of_journey' => 'required|date',
+            'journey_date' => 'required|string',
             'ticket_fare' => 'required|integer',
             'dep_time' => 'required|string',
             'arr_time' => 'required|string',
         ]);
 
-        // Create a new trip using Eloquent
-        Trip::create($validatedData);
-        return redirect()->route('bus.all-trip');
+        //Create a new trip using Eloquent
+        // $trip = new Trip();
+        // $trip->from = $validatedData["from"];
+        // $trip->to = $validatedData["to"];
+        // $trip->paribahan_name = $validatedData["paribahan_name"];
+        // $trip->total_seats = $validatedData["total_seats"];
+        // $trip->date_of_journey = $validatedData["journey_date"];
+        // $trip->ticket_fare = $validatedData["ticket_fare"];
+        // $trip->dep_time = $validatedData["dep_time"];
+        // $trip->arr_time = $validatedData["arr_time"];
+        // $trip->save();
+
+        $data = [
+            'from' => $request->from,
+            'to' => $request->to,
+            'paribahan_name' => $request->paribahan_name,
+            'total_seats' => $request->total_seats,
+            'date_of_journey' => $request->journey_date,
+            'ticket_fare' => $request->ticket_fare,
+            'dep_time' => $request->dep_time,
+            'arr_time' => $request->arr_time,
+        ];
+        Trip::create($data);
+        return redirect()->back();
 
     }
 
